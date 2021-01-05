e644f32f6d5a3e2b050b6e6b72e37403b1b87df4
rulixiang <rulixiang@outlook.com>
2021-01-05 15:28:13 +0800

    minor changes

--- run_deeplabv1.sh
+++ run_deeplabv1.sh
@@ -1,4 +1,4 @@
 ## train deeplabv1
-python v1/train_deeplabv1.py --gpu 6,7 --config config/deeplabv1_voc12.yaml
+python v1/train_deeplabv1.py --gpu 0,1,2 --config config/deeplabv1_voc12.yaml
 ## test on trained model
-python v1/test_deeplabv1.py --gpu 7 --crf True --config config/deeplabv1_voc12.yaml
+python v1/test_deeplabv1.py --gpu 0 --crf True --config config/deeplabv1_voc12.yaml
